251e70ece852a0afff8fa5d6c6900b9626b6dbbc
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2024-09-16 21:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+  // Trim any leading spaces and split the string by space
+  const trimmedStr = s.trim();
+  
+  // Find the index of the first space
+  const firstSpaceIdx = trimmedStr.indexOf(' ');
+  
+  // If there is no space, return the entire string
+  if (firstSpaceIdx === -1) {
+    return trimmedStr;
+  }
+  
+  // Otherwise, return the substring up to the first space
+  return trimmedStr.substring(0, firstSpaceIdx);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
